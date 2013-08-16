2cf59d45a1384d07e33f8a6f11fe1c77669f3173
EngineeringAwesome <robomower9508@gmail.com>
2013-08-16 12:16:04 -0500

    playing with variables and echo

--- index.php
+++ index.php
@@ -8,14 +8,32 @@ FTC Scouting
 
 <body>
 <?php
-echo "This is some php"
+echo "This is some php";
 ?>
 <div style="background-color:#E6B222; height:50px;"> <p> The Yellow Alliance </p> </div>
 <div> <p> This website is under construction, and will contain scouting data for teams in Iowa in the near future </p>
 
 
 <?php
-echo "This is some more php"
+	echo "This is some php that does math";
+	
+	function testFunction($x, $y) {
+		$z = $x + $y;
+		$w = $x * $y;
+
+		echo "<br>
+				 z = $x + $y = $z <br>
+				 w = $x * $y = $w ";
+	}
+	
+	function concatenate($txt1, $txt2) {
+		echo $txt1 . " plus " . $txt2 . ". Lengths: " . strlen($txt1) . ",  " . strlen($txt2);
+	}
+	
+	testFunction(2, 3);	//runs the function, testFunction
+	concatenate("Hello", "World");
+	
+
 ?>
 
 </body>
