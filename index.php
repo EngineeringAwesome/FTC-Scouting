54049ef582624a023ebcbd5dc714ff3a03fe540f
EngineeringAwesome <robomower9508@gmail.com>
2013-08-16 15:55:24 -0500

    added code to read from the team info file, and break it into array elements

--- index.php
+++ index.php
@@ -9,34 +9,26 @@
 	<body>
 	
 	<div style="background-color:#E6B222; height:50px;"> <p> The Yellow Alliance </p> </div>
-	<div> <p> This website is under construction, and will contain scouting data for teams in Iowa in the near future </p>
-	
-	<?php	
-		function testFunction($x, $y) {
-			$z = $x + $y;
-			$w = $x * $y;
-	
-			echo "<br>
-					 z = $x + $y = $z <br>
-					 w = $x * $y = $w ";
-		}
-		
-		function concatenate($txt1, $txt2) {
-			echo "<br>";
-			echo $txt1 . " plus " . $txt2 . ". Lengths: " . strlen($txt1) . ",  " . strlen($txt2);
-		}
-		
-	/*	function getDate() {
-			echo date("l");
-		} */
-				
-		echo "This is some php that does math";
-		testFunction(2, 3);	//runs the function, testFunction
-		echo "<br> This is some php that manipulates strings";
-		concatenate("Hello", "World");
-		getDate();
-		echo "<br>" . date("l") . "<br>";
-	?>
+	<div> <p> This website is under construction, and will contain scouting data for teams in Iowa in the near future </p> </div>
+	<div style="background-color:#aaaaaa;"> <p> Team Information </p>
+		<?php	
+			$teamInfo
+			$file=fopen("teamInfo.txt", "r") or exit("Unable to open file!");	//opens the file containg team info
+			
+			if ($file != 0) {	//if the team info file exists, skip the first line
+				fgets($file);
+				chop($file);
+				while(!feof($file)) {				//loop until the end of the file
+					$tempHolder = fgets($file);
+					$tempHolder = chop($tempHolder);	//gets rid of extra whitespace at the end
+					$temp = explode("|", $tempHolder, 4);
+					print_r($temp);
+					echo "<br>";					
+				}
+			}
+			fclose($file);
+		?>
+	</div>
 	
 	</body>
 </html>
