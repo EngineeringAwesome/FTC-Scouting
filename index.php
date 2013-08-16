8b20fbd7794292d7449151c495e3e9d91e0e3df0
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-08-16 10:02:57 -0500

    Test "hello world" style php code

--- index.php
+++ index.php
@@ -8,9 +8,15 @@ FTC Scouting
 
 <body>
 <?php
+echo "This is some php"
 ?>
 <div style="background-color:#E6B222; height:50px;"> <p> The Yellow Alliance </p> </div>
 <div> <p> This website is under construction, and will contain scouting data for teams in Iowa in the near future </p>
+
+
+<?php
+echo "This is some more php"
+?>
 
 </body>
 </html>
