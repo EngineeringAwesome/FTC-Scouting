198a191724bfa03d2b66d136eb7c9d6fa103704c
Engineering Awesome <engineeringawesome@Athena.(none)>
2013-08-16 09:58:28 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+FTC Scouting
+</title>
+</head>
+
+<body>
+<?php
+?>
+<div style="background-color:#E6B222; height:50px;"> <p> The Yellow Alliance </p> </div>
+<div> <p> This website is under construction, and will contain scouting data for teams in Iowa in the near future </p>
+
+</body>
+</html>
+
