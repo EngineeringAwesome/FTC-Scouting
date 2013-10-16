d27da86fb81996fd162f43b49aa89bb6c96f56e1
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-16 10:07:57 -0500

    added basic code to display the webpage template

--- events.php
+++ events.php
@@ -1,1 +1,25 @@
-blank file
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>FTC Scouting-Competitions</title>
+</head>
+
+<body>
+	<div id="header">
+    	<div style="background-color:#E6B222; height:50px;"> <h1> The Yellow Alliance </h1> </div>
+    	<nav>
+    		<a href="/index.php">Home</a>
+        	<a href="/teams.php">Teams</a>
+        	<a href="/events.php">Events</a>
+    	</nav>
+    </div>
+    
+	<div id="content">
+    	<p> This page will contain data from different competitions </p>
+	</div>
+    
+	<div id="footer">
+    </div>
+</body>
+</html>
